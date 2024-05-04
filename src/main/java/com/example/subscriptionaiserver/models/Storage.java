59c1fa77d6a87ddb64177dc7bc3be5d615388512
Thanusun Thayaparan <thanusun.thayaparan@mail.utoronto.ca>
2024-05-04 09:25:48 -0400

    Updated Application and pom files for database usage. Also database now hosted on azure.

--- src/main/java/com/example/subscriptionaiserver/models/Storage.java
+++ src/main/java/com/example/subscriptionaiserver/models/Storage.java
@@ -9,8 +9,26 @@ public class Storage {
 
     @Id
     @GeneratedValue
-    private Long storageId;
+    private int storageId;
     private String accessToken;
+    private String username;
+    private String encrPass;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEncrPass() {
+        return encrPass;
+    }
+
+    public void setEncrPass(String encrPass) {
+        this.encrPass = encrPass;
+    }
 
     public String getAccessToken() {
         return accessToken;
@@ -20,11 +38,11 @@ public class Storage {
         this.accessToken = accessToken;
     }
 
-    public void setStorageId(Long storageId) {
+    public void setStorageId(int storageId) {
         this.storageId = storageId;
     }
 
-    public Long getStorageId() {
+    public int getStorageId() {
         return storageId;
     }
 }
--- src/main/java/com/example/subscriptionaiserver/repositories/StorageRepo.java
+++ src/main/java/com/example/subscriptionaiserver/repositories/StorageRepo.java
@@ -1,11 +1,12 @@
 package com.example.subscriptionaiserver.repositories;
 
+import com.example.subscriptionaiserver.models.Storage;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface StorageRepo extends JpaRepository<Long, String> {
-    public List<String> findByAccessToken();
+public interface StorageRepo extends JpaRepository<Storage, Long> {
+    public List<String> findByAccessToken(String username);
 }
