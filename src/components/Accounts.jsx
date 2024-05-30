a9589c4e50405ff4f8f9d9492a9088976d8eb703
coolmanbat2 <thanusun.thayapara@mail.utoronto.ca>
2024-05-30 09:47:11 -0400

    Updated Frontend information to include link token.

--- src/components/Accounts.jsx
+++ src/components/Accounts.jsx
@@ -5,7 +5,54 @@ import { usePlaidLink } from "react-plaid-link";
 
 function Accounts() {
   async function accessBank() {
-    const details = await axios.post("/api/create_link_token");
+    const link = "http://localhost:8080";
+    const bodyData = {
+      user: {
+        client_user_id: "user-id",
+        phone_number: "416-643-4352"
+      },
+      client_name: "Personal Finance App",
+      products: ["transactions"],
+      transactions: {
+        days_requested: 640
+      },
+      country_codes: ["CA"],
+      language: "en",
+      redirect_uri: "https://localhost:5173",
+      account_filters: {
+        depository: {
+          account_subtypes: ["checking", "savings"]
+        }
+      },
+      credit: {
+        account_subtypes: ["credit card"]
+      }
+    }
+    console.log(bodyData)
+    
+    const public_token = await axios.get(link + "/link-token", {
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      },
+      // THIS IS A SAMPLE USER, PLEASE INSERT VALID USER AUTHENTICATION ONCE THIS CONNECTION PROCESS IS SETUP PROPERLLY. 
+      body: JSON.stringify(bodyData)
+        
+    })
+
+    console.log(public_token)
+
+    const details = await axios.post(link + "/exchange-token", {
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      },
+      data: {
+        'public_token': 'welkom'
+      }
+
+    }
+    );
   }
   return <div><button onClick={accessBank}>Click to access banking information! </button></div>;
 }
