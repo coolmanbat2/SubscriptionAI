0f4b3d121f724848bc8b43b9af7049319514e19c
Thanusun Thayaparan <thanusun.thayaparan@mail.utoronto.ca>
2024-05-31 11:58:09 -0400

    Made changes to account.jsx to ensure proper information is being uploaded.

--- src/components/Accounts.jsx
+++ src/components/Accounts.jsx
@@ -7,54 +7,78 @@ function Accounts() {
   async function accessBank() {
     const link = "http://localhost:8080";
     const bodyData = {
-      user: {
-        client_user_id: "user-id",
-        phone_number: "416-643-4352"
-      },
+      user: [{ client_user_id: "user-id" }, { phone_number: "416-643-4352" }],
       client_name: "Personal Finance App",
       products: ["transactions"],
-      transactions: {
-        days_requested: 640
-      },
+      transactions: [{ days_requested: 640 }],
       country_codes: ["CA"],
       language: "en",
       redirect_uri: "https://localhost:5173",
       account_filters: {
-        depository: {
-          account_subtypes: ["checking", "savings"]
-        }
+        depository: [{ account_subtypes: ["checking", "savings"] }],
       },
-      credit: {
-        account_subtypes: ["credit card"]
+      credit: [{ account_subtypes: ["credit card"] }],
+    };
+    console.log(bodyData);
+
+    const public_token = await axios.post(link + "/link-token", {
+      // headers: {
+      // 'Content-Type': 'application/json',
+      // 'Access-Control-Allow-Origin': '*'
+      // }}, {
+      // THIS IS A SAMPLE USER, PLEASE INSERT VALID USER AUTHENTICATION ONCE THIS CONNECTION PROCESS IS SETUP PROPERLLY.
+      user: [
+        {
+          client_user_id: "user-id",
+        },
+        {
+          phone_number: "416-643-4352",
+        },
+      ],
+      client_name: "Personal Finance App",
+      products: ["transactions"],
+      transactions: [
+        {
+          days_requested: 640,
+        },
+      ],
+      country_codes: ["CA"],
+      language: "en",
+      redirect_uri: "https://localhost:5173",
+      account_filters: {
+        depository: [
+          {
+            account_subtypes: ["checking", "savings"],
+          },
+        ],
+      },
+      credit: [
+        {
+          account_subtypes: ["credit card"],
+        },
+      ],
+    });
+
+    console.log(public_token);
+
+    const details = await axios.post(
+      link + "/exchange-token",
+      // headers: {
+      // 'Content-Type': 'application/json',
+      // 'Access-Control-Allow-Origin': '*'
+      // },
+      {
+        public_token: "welkom",
       }
-    }
-    console.log(bodyData)
-    
-    const public_token = await axios.get(link + "/link-token", {
-      headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
-      },
-      // THIS IS A SAMPLE USER, PLEASE INSERT VALID USER AUTHENTICATION ONCE THIS CONNECTION PROCESS IS SETUP PROPERLLY. 
-      body: JSON.stringify(bodyData)
-        
-    })
-
-    console.log(public_token)
-
-    const details = await axios.post(link + "/exchange-token", {
-      headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
-      },
-      data: {
-        'public_token': 'welkom'
-      }
-
-    }
     );
   }
-  return <div><button onClick={accessBank}>Click to access banking information! </button></div>;
+  return (
+    <div>
+      <button onClick={accessBank}>
+        Click to access banking information!{" "}
+      </button>
+    </div>
+  );
 }
 
 export default Accounts;
